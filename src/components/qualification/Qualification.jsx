b24f39521fa90cd64bcd92f8ad81fc6ea73bd7cf
Shaheryar <shaheryarfaraz9@gmail.com>
2023-05-16 00:47:02 +0500

    second commit

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -5,15 +5,14 @@ const Qualification = () => {
   return (
     
     <section className="qualification section" id="qualification">
-      <h2 className="section__title">Qualification</h2>
+      <h2 className="section__title">Experience</h2>
       <div className="main-div">
       <div className="qualification__container container">
         <div className="box-container left">
          <div className="text-box">
-            <h2>Alpha Inc</h2>
+            <h2>CLOUD PRIMERO</h2>
             <small>2018-2019</small>
-            <p>the success of every wevsite depends on search engine optimization and digital marketing strategy.
-                If you are on first page of all major search..
+            <p>WEB DEVELOPER  
             </p>
             <span className='left-arrow'></span>
             
@@ -23,10 +22,9 @@ const Qualification = () => {
 
         <div className="box-container right">
          <div className="text-box">
-            <h2>google</h2>
-            <small>2018-2019</small>
-            <p>the success of every wevsite depends on search engine optimization and digital marketing strategy.
-                If you are on first page of all major search..
+            <h2>TRADEMOR MARKETING </h2>
+            <small>2020-2021</small>
+            <p>TEAM LEAD - ACCOUNT PERFORMANCE & MANAGEMENT (ALIBABA INC)
             </p>
             <span className='right-arrow'></span>
          </div>
@@ -35,10 +33,9 @@ const Qualification = () => {
 
         <div className="box-container left">
          <div className="text-box">
-            <h2>Amazon</h2>
-            <small>2012-2016</small>
-            <p>the success of every wevsite depends on search engine optimization and digital marketing strategy.
-                If you are on first page of all major search..
+            <h2>Zaviya Solutions</h2>
+            <small>2022-2023</small>
+            <p> React Developer 
             </p>
             <span className='left-arrow'></span>
             
@@ -46,7 +43,7 @@ const Qualification = () => {
          <span className='left-dot2'></span>
         </div>
 
-        <div className="box-container right">
+        {/* <div className="box-container right">
          <div className="text-box">
             <h2>Alpha Inc</h2>
             <small>2021-2023</small>
@@ -56,7 +53,7 @@ const Qualification = () => {
             <span className='right-arrow'></span>
          </div>
          <span className='right-dot2'></span>
-        </div>
+        </div> */}
       </div>
       </div>
  </section>
