f3579ebb6ace2f1a2324cd49f04a9bd3308f7b06
Shaheryar <shaheryarfaraz9@gmail.com>
2023-12-10 22:21:34 +0500

    Header, data changes

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -8,12 +8,13 @@ export const projectData = [
     image:Random,
     title:"Social App",
     category:"App",
+    links:"https://github.com/shery311/Social-App-project"
 },
 
 {
     id:2,
-    image:Random,
-    title:"dashboard",
+    image:"https://images.pexels.com/photos/18221948/pexels-photo-18221948/free-photo-of-beautiful-brunette-woman-in-white-off-the-shoulder-dress-standing-on-a-beach.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title:"CRUD API",
     category:"RestApi",
     links:"https://github.com/shery311/CRUD-API",
 },
--- src/components/projects/WorksItems.jsx
+++ src/components/projects/WorksItems.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 export const WorksItems = ({item}) => {
@@ -6,9 +7,9 @@ export const WorksItems = ({item}) => {
     <div className="work__card" key={item.id}>
      <img src={item.image} alt="" className='work__img'/>
      <h3 className="work__title">{item.title}</h3>
-     <a href={`${item.link}`} target='_blank' rel="noreferrer" className="work__button">
+     <Link to={item.links} target='_blank' rel="noreferrer" className="work__button"> 
         Demo <div className="i bx bx-right-arrow-alt work__button-icon"></div>
-     </a>
+     </Link>
     </div>
   )
 }
